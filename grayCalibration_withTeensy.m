cb8e685088e4d05b0fdb71944e96c36354f1c0dc
spoudel4 <spoudel4@ur.rochester.edu>
2025-02-24 10:21:46 -0500

    Update grayCalibration_withTeensy.m
    Base code to control LEDs and call stimuli as functions

--- grayCalibration_withTeensy.m
+++ grayCalibration_withTeensy.m
@@ -14,32 +14,7 @@ lights.On_s = 10;
 lights.Off_s = 0.2;
 
 lights.start_with_defaults()
+ 
+% call function for grayscale calibration
 
-tic
-while toc < 1.003 % this is the delay we measured for teensy light source
-
-end
-
- function grayscaleStim
-%% Suppress Screen Warning
-Screen('Preference', 'SkipSyncTests', 0);  
-Screen('Preference','VisualDebugLevel',1);  
-Screen('Preference','SuppressAllWarnings',0);
-%% Set basic parameters
-ScreenValue=2;
-[WindowIndex,WindowRect] = Screen('OpenWindow', ScreenValue);
-
-    
-
-%% Define Grayscale Value Array
-nValues=15;
-grayValues=round(linspace(0,255,nValues));
-
-%% Display White Intensity Screens
-for i=1:length(grayValues)
-Screen('FillRect',WindowIndex,[grayValues(i) grayValues(i) grayValues(i)]);
-Screen('Flip', WindowIndex);
-     KbWait([], 2); 
-end 
-Screen('CloseAll')
-end  
+grayscaleStim
